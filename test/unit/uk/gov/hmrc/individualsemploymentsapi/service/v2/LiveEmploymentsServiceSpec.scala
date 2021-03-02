17cb17719f6455aca693a6a21e43f6397f3b25bf
owengilfellon <owen.gilfellon@opencastsoftware.com>
2021-03-02 17:34:03 +0000

    Fixed compiler error

--- test/unit/uk/gov/hmrc/individualsemploymentsapi/service/v2/LiveEmploymentsServiceSpec.scala
+++ test/unit/uk/gov/hmrc/individualsemploymentsapi/service/v2/LiveEmploymentsServiceSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package unit.uk.gov.hmrc.individualsemploymentsapi.service.v2
 
 import org.scalatestplus.mockito.MockitoSugar.mock
@@ -6,8 +22,10 @@ import uk.gov.hmrc.individualsemploymentsapi.connector.{IfConnector, Individuals
 import uk.gov.hmrc.individualsemploymentsapi.service.v2.{CacheIdBase, CacheService, LiveEmploymentsService, ScopesHelper}
 import unit.uk.gov.hmrc.individualsemploymentsapi.util.SpecBase
 import utils.Intervals
-
 import java.util.UUID
+
+import uk.gov.hmrc.http.HeaderCarrier
+
 import scala.concurrent.Future
 
 class LiveEmploymentsServiceSpec extends SpecBase with Intervals {
@@ -17,8 +35,7 @@ class LiveEmploymentsServiceSpec extends SpecBase with Intervals {
     implicit val ec = scala.concurrent.ExecutionContext.global
 
     val mockCacheService = new CacheService(null, null)(null) {
-      override def get[T: Format](cacheId: CacheIdBase,
-                                  functionToCache: => Future[T]) =
+      override def get[T: Format](cacheId: CacheIdBase,functionToCache: => Future[T])(implicit hc: HeaderCarrier) =
         functionToCache
     }
 
