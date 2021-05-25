395a82ad44e038342280ea35d54b672ab1310eb4
owengilfellon <owen.gilfellon@opencastsoftware.com>
2021-05-25 11:14:58 +0100

    Fix for failing test

--- test/component/uk/gov/hmrc/individualsemploymentsapi/stubs/IfStub.scala
+++ test/component/uk/gov/hmrc/individualsemploymentsapi/stubs/IfStub.scala
@@ -27,8 +27,7 @@ object IfStub extends MockHost(22004) {
   val fieldsAndFilters = List[(String, Option[String])](
     ("employments(employer(address(line1,line2,line3,line4,line5,postcode),name),employment(startDate))", None),
     ("employments(employer(address(line1,line2,line3,line4,line5,postcode),districtNumber,name,schemeRef),employerRef,employment(endDate,startDate),payments(date,paidTaxablePay))", None),
-    ("employments(employer(address(line1,line2,line3,line4,line5,postcode),districtNumber,name,schemeRef),employerRef,employment(endDate,startDate),payments(date,paidTaxablePay))", Some("employments%5B%5D/employerRef%20eq%20'%3CemployerRef%3E'")),
-    ("employments(employer(address(line1,line2,line3,line4,line5,postcode),districtNumber,name,schemeRef),employerRef,employment(endDate,payFrequency,startDate),payments(date,paidTaxablePay))", Some("employments%5B%5D/employerRef%20eq%20'247ZT6767895A'"))
+    ("employments(employer(address(line1,line2,line3,line4,line5,postcode),districtNumber,name,schemeRef),employerRef,employment(endDate,payFrequency,startDate),payments(date,paidTaxablePay))", Some("employments[]/employerRef eq '247ZT6767895A'"))
   )
 
   def searchEmploymentIncomeForPeriodReturns(
@@ -37,26 +36,18 @@ object IfStub extends MockHost(22004) {
     toDate: String,
     ifEmployments: IfEmployments) = {
     fieldsAndFilters.foreach((fieldFilter) => {
-      println("ff: " + fieldFilter._2)
-      fieldFilter._2 match {
-        case None => mock.register(
+       mock.register(
           get(urlPathEqualTo(s"/individuals/employment/nino/$nino"))
             .withQueryParam("startDate", equalTo(fromDate))
             .withQueryParam("endDate", equalTo(toDate))
             .withQueryParam("fields", equalTo(fieldFilter._1))
+            .withQueryParam("filter",
+              fieldFilter._2 match {
+                case None => absent()
+                case Some(filter) => equalTo(filter)
+              })
             .willReturn(aResponse().withStatus(OK).withBody(Json.toJson(ifEmployments).toString())))
-        case Some(filter) => {
-          mock.register(
-            get(urlPathEqualTo(s"/individuals/employment/nino/$nino"))
-              .withQueryParam("startDate", equalTo(fromDate))
-              .withQueryParam("endDate", equalTo(toDate))
-              .withQueryParam("fields", equalTo(fieldFilter._1))
-              .withQueryParam("filter", equalTo(filter))
-              .willReturn(aResponse().withStatus(OK).withBody(Json.toJson(ifEmployments).toString())))
-        }
-      }
     })
-    println("stubs: " + mock.allStubMappings().getMappings)
   }
 
 
