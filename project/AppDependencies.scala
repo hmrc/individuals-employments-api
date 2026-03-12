3c6179dab84a3bb3e11e1c3cfa1e36f654d4ced4
gajalakshmiseetharaman <177637840+gajalakshmiseetharaman@users.noreply.github.com>
2026-03-12 11:16:08 +0000

    DLS-12764|Bootstrap-version-upgraded (#168)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
   val hmrc = "uk.gov.hmrc"
   val hmrcMongo = s"$hmrc.mongo"
   val mongoVersion = "2.12.0"
-  val bootstrapVersion = "10.5.0"
+  val bootstrapVersion = "10.7.0"
   val playVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
